f0331d62fb66436932fb4b0f0b9423d2dbbbcc26
Liviu Ionescu <ilg@livius.net>
2023-06-30 22:18:20 +0300

    cosmetics

--- include/cmsis-plus/rtos/port/os-decls.h
+++ include/cmsis-plus/rtos/port/os-decls.h
@@ -26,6 +26,7 @@
 #if defined(OS_USE_OS_APP_CONFIG_H)
 #include <cmsis-plus/os-app-config.h>
 #endif
+
 #include <cmsis-plus/rtos/port/os-c-decls.h>
 
 // ----------------------------------------------------------------------------
--- src/diag/trace-posix.cpp
+++ src/diag/trace-posix.cpp
@@ -19,6 +19,7 @@
 #if defined(OS_USE_OS_APP_CONFIG_H)
 #include <cmsis-plus/os-app-config.h>
 #endif
+
 #include <cmsis-plus/diag/trace.h>
 #include <cstddef>
 #include <unistd.h>
